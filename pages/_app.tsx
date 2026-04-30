e921f97838efa50c6cd9820f31b775298a126693
charlesbb88-ux <charlesbb88@gmail.com>
2026-04-30 03:14:01 -0500

    tutorial global

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import Head from "next/head";
 import Header from "../components/Header";
 import AuthModalManager from "../components/AuthModalManager";
 import UsernameGate from "../components/UsernameGate";
+import OnboardingTutorialModal from "../components/OnboardingTutorialModal";
 
 import { Analytics } from "@vercel/analytics/react";
 
@@ -45,6 +46,7 @@ export default function App({ Component, pageProps }: AppPropsWithHeader) {
         </div>
 
         <AuthModalManager />
+        <OnboardingTutorialModal />
         <Analytics />
       </>
     </UsernameGate>
